934c3ee7a31a3a28d66b6f41a187904134186977
thiagop127001 <thiagogua2009@gmail.com>
2022-11-02 09:52:24 -0300

    create authentication by jetstream

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,18 @@ use App\Http\Livewire\{
     ShowTweets
 };
 
-Route::get('tweets', ShowTweets::class);
+Route::get('tweets', ShowTweets::class)->middleware('auth')->name('tweets.show');
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
